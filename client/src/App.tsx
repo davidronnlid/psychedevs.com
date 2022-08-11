d1efd3121ba030cd5a84e25e44b5fd89a423f185
David Rönnlid <david.ronnlid@gmail.com>
2022-08-11 22:24:47 +0200

    client

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <a href="/login">Login</a>
+      <button>Fetch user</button>
+      No longer a git repo...
+    </div>
+  );
+}
+
+export default App;
