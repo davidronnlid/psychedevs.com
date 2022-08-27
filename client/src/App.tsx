c777568161b28ea4352fa7700734a5b8fa28dc5c
David Rönnlid <david.ronnlid@gmail.com>
2022-08-27 16:29:20 +0200

    React client with working Oauth + MongoDB backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,35 @@ import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
-import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-import Create from "./components/create";
+// import RecordList from "./components/recordList";
+// import Edit from "./components/edit";
+// import Create from "./components/create";
+
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [usersInDB, setUsersInDB] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/logged-in-user")
+      .then((res) => {
+        console.log("Logging res", res.json(), res);
+
+        return res;
+      })
+      .then((data) => setUsersInDB(data.json().body.toString()));
+  }, []);
+
+  // Goal is to
+
   return (
     <div>
       <Navbar />
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<RecordList />} />
         <Route path="/edit/:id" element={<Edit />} />
         <Route path="/create" element={<Create />} />
-      </Routes>
+      </Routes> */}
       <div
         style={{
           margin: "0 auto",
@@ -26,10 +42,11 @@ const App = () => {
           justifyContent: "space-around",
         }}
       >
-        <a href="/login">Login</a>
+        <a href="http://localhost:8000/login">Login</a>
         <button>Fetch user</button>
-        No longer a git repo...
       </div>
+      <h1>Logged in user</h1>
+      <>{usersInDB}</>
     </div>
   );
 };
